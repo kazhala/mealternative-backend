2bb1c271f373daea07885f96fd1617dc4d1dfa57
kevin zhuang <kevin7441@gmail.com>
2020-01-01 10:20:16 +1100

    set up express and dotenv

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Sever is running on port ${port}`);
+});
