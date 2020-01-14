037c92ace1d23a4060a52851a46319ccc8ca4e89
kevin zhuang <kevin7441@gmail.com>
2020-01-14 11:24:46 +1100

    create category

--- controllers/category.js
+++ controllers/category.js
@@ -5,8 +5,16 @@ const Category = require('../models/category');
 
 module.exports.createCategory = async (req, res) => {
   try {
-    console.log(req.profile);
+    const { name } = req.body;
+    let category = new Category({ name });
+    await category.save();
+    return res.status(200).json({
+      message: 'Category successfully created'
+    });
   } catch (err) {
     console.log(err);
+    return res.status(500).json({
+      error: 'Something went wrong'
+    });
   }
 };
