fd58c777ba0466a1486af2c542ac4981eb3e39ae
kevin zhuang <kevin7441@gmail.com>
2020-02-25 12:09:08 +1100

    pagination on bookmarks

--- controllers/user.js
+++ controllers/user.js
@@ -108,13 +108,22 @@ module.exports.readUser = async (req, res) => {
 // retrieve user bookmarks
 module.exports.listUserBookmarks = async (req, res) => {
   const userId = req.params.userId;
+  const size = req.query.size ? Number(req.query.size) : 10;
+  const page = req.query.page ? Number(req.query.page) : 1;
+  const skip = (page - 1) * size;
   try {
-    const bookmarks = await Bookmark.find({ user: userId }).populate({
-      path: 'recipe',
-      select: 'thumbImageUrl likes bookmarks rating title description postedBy',
-      populate: { path: 'postedBy', select: 'username photoUrl' }
-    });
-    return res.status(200).json(bookmarks);
+    const totalEntries = await Bookmark.count({ user: userId });
+    const totalPages = Math.ceil(totalEntries / size);
+    const bookmarks = await Bookmark.find({ user: userId })
+      .populate({
+        path: 'recipe',
+        select:
+          'thumbImageUrl likes bookmarks rating title description postedBy',
+        populate: { path: 'postedBy', select: 'username photoUrl' }
+      })
+      .limit(size)
+      .skip(skip);
+    return res.status(200).json({ bookmarks, page, totalPages });
   } catch (err) {
     console.log('Error', err);
     return res.status(500).json({
