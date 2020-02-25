79cacd4995c2b0e3f8166bb737abd5cdc7743066
kevin zhuang <kevin7441@gmail.com>
2020-02-25 12:29:29 +1100

    sort bookmarks based on creation time

--- controllers/user.js
+++ controllers/user.js
@@ -108,6 +108,7 @@ module.exports.readUser = async (req, res) => {
 // retrieve user bookmarks
 module.exports.listUserBookmarks = async (req, res) => {
   const userId = req.params.userId;
+  const orderBy = req.query.orderBy ? req.query.orderBy : '-createdAt';
   const size = req.query.size ? Number(req.query.size) : 10;
   const page = req.query.page ? Number(req.query.page) : 1;
   const skip = (page - 1) * size;
@@ -121,6 +122,7 @@ module.exports.listUserBookmarks = async (req, res) => {
           'thumbImageUrl likes bookmarks rating title description postedBy',
         populate: { path: 'postedBy', select: 'username photoUrl' }
       })
+      .sort(orderBy)
       .limit(size)
       .skip(skip);
     return res.status(200).json({ bookmarks, page, totalPages });
--- models/bookmark.js
+++ models/bookmark.js
@@ -6,15 +6,18 @@
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema;
 
-const bookmarkSchema = new mongoose.Schema({
-  user: {
-    type: ObjectId,
-    ref: 'User'
+const bookmarkSchema = new mongoose.Schema(
+  {
+    user: {
+      type: ObjectId,
+      ref: 'User'
+    },
+    recipe: {
+      type: ObjectId,
+      ref: 'Recipe'
+    }
   },
-  recipe: {
-    type: ObjectId,
-    ref: 'Recipe'
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Bookmark', bookmarkSchema);
