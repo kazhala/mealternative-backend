de73b3cde4b63b942f6662689351930833da2dff
kevin zhuang <kevin7441@gmail.com>
2020-04-14 08:48:12 +1000

    update recipe constraints

--- validators/recipe.js
+++ validators/recipe.js
@@ -2,26 +2,20 @@ const { check } = require('express-validator');
 
 module.exports.recipeCreateValidator = [
   check('title')
-    .isLength({ min: 3, max: 60 })
-    .withMessage('Title needs to be between 3-60 characters'),
-  check('description')
-    .isLength({ max: 1000 })
-    .withMessage("Description can't be longer than 1000"),
+    .isLength({ min: 3 })
+    .withMessage('Title needs to be at least 3 characters'),
   check('categories')
     .isArray({ min: 1 })
     .withMessage('At least one category should be selected'),
   check('ingredients')
-    .isLength({ max: 30 })
+    .isLength({ max: 100 })
     .withMessage('Length should not exceeds 30'),
   check('steps')
     .isArray({ min: 1 })
     .withMessage('Should have at least one step'),
   check('steps.*.stepTitle')
-    .isLength({ min: 3, max: 60 })
+    .isLength({ min: 3 })
     .withMessage('Each step title should be within 3 and 60'),
-  check('steps.*.stepDescriptions')
-    .isLength({ max: 1000 })
-    .withMessage('Step description should be less than 1000')
 ];
 
 module.exports.recipeUpdateValidator = [
@@ -29,30 +23,22 @@ module.exports.recipeUpdateValidator = [
     .isURL()
     .withMessage("Can't resolve the thumbnail image url"),
   check('title')
-    .isLength({ min: 3, max: 60 })
-    .withMessage('Title needs to be between 3-60 characters'),
-  check('description')
-    .isLength({ max: 1000 })
-    .withMessage("Description can't be longer than 1000"),
+    .isLength({ min: 3 })
+    .withMessage('Title needs to be at least 3 characters'),
   check('categories')
     .isArray({ min: 1 })
     .withMessage('At least one category should be selected'),
   check('ingredients')
-    .isLength({ max: 30 })
-    .withMessage('Length should not exceeds 30'),
+    .isLength({ max: 100 })
+    .withMessage('Length should not exceeds 100'),
   check('steps')
     .isArray({ min: 1 })
     .withMessage('Should have at least one step'),
   check('steps.*.stepTitle')
-    .isLength({ min: 3, max: 60 })
-    .withMessage('Each step title should be within 3 and 60'),
-  check('steps.*.stepDescriptions')
-    .isLength({ max: 1000 })
-    .withMessage('Step description should be less than 1000')
+    .isLength({ min: 3 })
+    .withMessage('Each step title should be at least 3 characters'),
 ];
 
 module.exports.recipeRatingValidator = [
-  check('rating')
-    .isNumeric()
-    .withMessage('Must enter a rating number')
+  check('rating').isNumeric().withMessage('Must enter a rating number'),
 ];
